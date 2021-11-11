47cfc9f5983500ccc676142afc139730e76290ab
chichafon <chichafonchichafon@gmail.com>
2021-11-11 08:43:56 +0400

    Initial Commit

---
+++ Homework10-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework10-project
+//
+//  Created by 111 on 11.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
